99f95b05fea8474b977461387e35a2a51255d2ff
Dan Cigrang <danielcigrang@me.com>
2026-08-26 15:18:31 -0500

    leftover artifacts

--- src/index.ts
+++ src/index.ts
@@ -16,29 +16,3 @@ if (env.ENABLE_SERVER) {
   startServer(env.PORT);
   logger.info(`Server running at http://localhost:${env.PORT}`);
 }
-
-// import { z } from 'zod';
-
-// export const envSchema = z.object({
-//   LOG_LEVEL: z.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace']),
-//   NODE_ENV: z.enum(['development', 'production']).default('development'),
-//   PORT: z.coerce.number().default(3000),
-//   PRINTER_HOST: z.string().default('10.0.1.128'),
-//   PRINTER_OFFLINE: z.enum(['true', 'false']).default('false'),
-//   CLIP_API_SECRET: z.string().optional(),
-// });
-
-// export type Env = z.infer<typeof envSchema>;
-
-// const env: Env = (() => {
-//   try {
-//     return envSchema.parse(process.env);
-//   } catch (error) {
-//     const zodError = error as z.ZodError;
-//     console.error('❌ Invalid environment variables:');
-//     console.error(z.prettifyError(zodError));
-//     process.exit(1);
-//   }
-// })();
-
-// export default env;
